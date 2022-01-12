09a0f028d888891f06b425615e662cd7fecacd26
dave_id <djanur405@gmail.com>
2022-01-12 14:40:18 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Demo : https://quotes-generator-simply.netlify.app/
+
+Build with:
+HTML
+CSS
+Javascript
+
+Source:
+Font: Google Fonts (https://fonts.google.com)
+Icons: fontawesome (fontawesome.com)
+API: https://github.com/lukePeavey/quotable
+
+Referensi: 
+Youtube: https://www.youtube.com/watch?v=I0v9ZOY3_0k
+
+Thanks
+
+
+Date: 12/01/2022
---
+++ script.js
@@ -0,0 +1,41 @@
+const quoteText = document.querySelector('.quote'),
+  authorName = document.querySelector('.author .name'),
+  quoteBtn = document.querySelector('button'),
+  soundBtn = document.querySelector('.sound'),
+  copyBtn = document.querySelector('.copy'),
+  twitterBtn = document.querySelector('.twitter');
+
+// random quote function
+function randomQuote() {
+  quoteBtn.classList.add('loading');
+  quoteBtn.innerText = 'Loading Quote...';
+  // fatching random quotes/data from the API and parsing  it into Javascript object
+  fetch('http://api.quotable.io/random')
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      quoteText.innerText = result.content;
+      authorName.innerText = result.author;
+      quoteBtn.innerText = 'New Quote';
+      quoteBtn.classList.remove('loading');
+    });
+}
+
+soundBtn.addEventListener('click', () => {
+  // the SpeechSynthesisUtterance is a web speech api that represents a speech request
+  let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+  speechSynthesis.speak(utterance); //speak method  os speechSynthesis speaks the utterance
+});
+
+copyBtn.addEventListener('click', () => {
+  // copying the quote text on copyBtn click
+  //   writeText() property writes the specified text string to the system clipboard.
+  navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener('click', () => {
+  let tweetrUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+  window.open(tweetrUrl, '_blank'); //opening a new twitter tab with passing quote in the url
+});
+
+quoteBtn.addEventListener('click', randomQuote);
